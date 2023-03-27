6b33ccbb788482830a0d7aae6223d994e842ebd5
ingazhur <ingazhuravlev@gmail.com>
2023-03-27 15:29:09 -0700

    preliminary real-time streaming is working

--- dashboard/dash.py
+++ dashboard/dash.py
@@ -1,121 +1,129 @@
 import sys
-import csv
 import serial
-import threading
+import csv
+import os
+import time
 import numpy as np
-import pyqtgraph as pg
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-import serial.tools.list_ports
+import pyqtgraph as pg
 
-class WaveformReader(QWidget):
+def populate_serial_ports(self):
+    # Use the "ls /dev/cu.*" command to get a list of serial ports
+    available_ports = []
+    # run 'ls /dev/cu.*' in terminal
+    # get the output of the command
+    # split the output into a list and store it in available_ports
+    available_ports = os.popen('ls /dev/cu.*').read().split()
+    # add the available ports to the combo box
+    for port in available_ports:
+        self.serial_port_combo.addItem(port)
+
+
+class EMG_GUI(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        
         # Initialize GUI elements
-        self.serial_port = QComboBox(self)
-        self.serial_port_label = QLabel('Serial Port', self)
-        self.channel_count = QComboBox(self)
-        self.channel_count_label = QLabel('Channel Count', self)
-        self.start_button = QPushButton('Start Recording', self)
-        self.stop_button = QPushButton('Stop Recording', self)
+        self.serial_port_combo = QComboBox(self)
+        self.record_button = QPushButton("Start Recording", self)
         self.plot_widget = pg.PlotWidget(self)
+        self.plot_widget.setYRange(0, 1024) # prevent scaling
         self.plot_curve = None
-        self.csv_data = []
-        self.csv_file = None
 
         # Set GUI layout
         layout = QGridLayout()
-        layout.addWidget(self.serial_port_label, 0, 0)
-        layout.addWidget(self.serial_port, 0, 1)
-        layout.addWidget(self.channel_count_label, 1, 0)
-        layout.addWidget(self.channel_count, 1, 1)
-        layout.addWidget(self.start_button, 2, 0)
-        layout.addWidget(self.stop_button, 2, 1)
-        layout.addWidget(self.plot_widget, 3, 0, 1, 2)
-        self.setLayout(layout)
+        layout.addWidget(self.serial_port_combo, 0, 0)
+        layout.addWidget(self.record_button, 0, 1)
+        layout.addWidget(self.plot_widget, 1, 0, 1, 2)
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
 
-        # Populate serial port dropdown
-        for port in serial.tools.list_ports.comports():
-            self.serial_port.addItem(port.device)
+        # Create a combo box for selecting the serial port
+        self.serial_port_combo.addItem("Select Serial Port")
+        self.populate_serial_ports()
+        self.serial_port_combo.currentIndexChanged.connect(self.update_serial_port)
+        # port for serial communication with the device
+        self.port = None
 
-        # Populate channel count dropdown
-        self.channel_count.addItems(['1', '2', '3', '4', '5'])
-
-        # Connect signals to slots
-        self.start_button.clicked.connect(self.start_recording)
-        self.stop_button.clicked.connect(self.stop_recording)
-        self.serial_port.currentIndexChanged.connect(self.update_serial_port)
-        self.channel_count.currentIndexChanged.connect(self.update_channel_count)
+        # Create a start/stop recording button
+        self.record_button.setStyleSheet("background-color: green; color: white")
+        self.record_button.clicked.connect(self.toggle_record)
 
         # Set window properties
+        self.setWindowTitle('Neurotech Devices - EMG analysis GUI')
         self.setGeometry(300, 300, 800, 600)
-        self.setWindowTitle('Waveform Reader')
 
-        # Initialize serial connection
-        self.ser = None
-        self.serial_thread = None
-        self.serial_running = False
-        self.serial_lock = threading.Lock()
+    def populate_serial_ports(self):
+        # Use the "ls /dev/cu.*" command to get a list of serial ports
+        available_ports = []
+        # run 'ls /dev/cu.*' in terminal
+        # get the output of the command
+        # split the output into a list and store it in available_ports
+        available_ports = os.popen('ls /dev/cu.*').read().split()
+        # add the available ports to the combo box
+        for port in available_ports:
+            self.serial_port_combo.addItem(port)
 
     def update_serial_port(self):
-        # Update serial port when dropdown selection changes
-        port_name = self.serial_port.currentText()
-        if self.ser:
-            self.ser.close()
-        self.ser = serial.Serial(port_name, baudrate=115200)
+        # Connect to the selected serial port and start streaming data
+        self.port = self.serial_port_combo.currentText()
+        if self.port != "Select Serial Port":
+            self.ser = serial.Serial(self.port, 9600, timeout=1)
+            self.record_button.setEnabled(True)
 
-    def update_channel_count(self):
-        # Update channel count when dropdown selection changes
-        channel_count = int(self.channel_count.currentText())
-        self.plot_widget.clear()
-        self.plot_curve = self.plot_widget.plot(pen='b')
-        self.plot_curve.setData(np.zeros(channel_count))
+    def toggle_record(self):
+        # Start or stop recording data to a CSV file
+        if self.record_button.text() == "Start Recording":
+            self.record_button.setText("Stop Recording")
+            self.record_button.setStyleSheet("background-color: red; color: white")
+            self.csv_file = open("emg_data.csv", "w", newline="")
+            self.csv_writer = csv.writer(self.csv_file)
+            self.csv_writer.writerow(["Time", "EMG Data"])
+            self.record_data = True
+            self.start_time = time.time()
+            self.plot_real_time_data()
+        else:
+            self.record_button.setText("Start Recording")
+            self.record_button.setStyleSheet("background-color: green; color: white")
+            self.csv_file.close()
+            self.record_data = False
 
-    def start_recording(self):
-        # Start recording when button is pressed
-        self.csv_data = []
-        self.csv_file = open('data.csv', 'w', newline='')
-        self.serial_running = True
-        self.serial_thread = threading.Thread(target=self.serial_reader)
-        self.serial_thread.start()
+    def plot_real_time_data(self):
+        # This function will plot the data from the selected serial port in real-time
+        ser = self.ser
+        self.xdata, self.ydata = [], []  # Initialize the x and y data arrays
+        self.record_data = False  # Set the record data flag to False
+        self.start_time = time.time()  # Get the start time
+        # Create the the plot curve that updates in real-time every 10ms
+        self.plot_curve = self.plot_widget.plot(self.xdata, self.ydata, pen=pg.mkPen('r', width=3))
+        self.timer = pg.QtCore.QTimer()
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start(10)
 
-    def stop_recording(self):
-        # Stop recording when button is pressed
-        self.serial_running = False
-        self.serial_thread.join()
-        self.csv_file.close()
-
-    def serial_reader(self):
-        # Read data from serial port and update plot in real-time
-        channel_count = int(self.channel_count.currentText())
-        while self.serial_running:
-            # Read data from serial port
-            with self.serial_lock:
-                data = self.ser.readline().decode().rstrip().split(',')
-            if len(data) == channel_count:
-                data = [float(x) for x in data]
-                self.csv_data.append(data)
-                self.csv_file.write(','.join([str(x) for x in data]) + '\n')
-                self.plot_curve.setData(data)
-
-def read_csv_file(self):
-    # Read CSV file and display data in plot
-    file_name, _ = QFileDialog.getOpenFileName(self, 'Open CSV file', '', 'CSV files (*.csv)')
-    if file_name:
-        with open(file_name, 'r') as file:
-            reader = csv.reader(file)
-            data = []
-            for row in reader:
-                row = [float(x) for x in row]
-                data.append(row)
-            data = np.array(data).T
-            self.channel_count.setCurrentIndex(data.shape[0] - 1)
-            self.plot_curve.setData(data)
-
-if __name__ == '__main__':
+    def update_plot_data(self):
+        # This function will update the plot data in real-time
+        ser = self.ser
+        # Read the data from the serial port
+        data = ser.readline().decode("utf-8")
+        # Check if the data is valid
+        if data != "":
+            # Get the current time
+            current_time = time.time() - self.start_time
+            # Add the data to the x and y data arrays
+            self.xdata.append(current_time)
+            self.ydata.append(float(data))
+            # Update the plot curve with the new data (only keep the last 100 data points)
+            self.plot_curve.setData(self.xdata[-100:], self.ydata[-100:])
+            # Check if the record data flag is set to True
+            if self.record_data:
+                # Write the data to the CSV file
+                self.csv_writer.writerow([current_time, float(data)])
+        
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    reader = WaveformReader()
-    reader.show()
+    gui = EMG_GUI()
+    gui.show()
     sys.exit(app.exec_())
